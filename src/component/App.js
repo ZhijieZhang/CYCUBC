a61bcc48b53e36736ae520e9b7696d25adb87a16
Zhijie Zhang <ZhijieZhang0124@gmail.com>
2018-05-07 22:21:07 -0700

    active button

--- src/component/App.js
+++ src/component/App.js
@@ -5,11 +5,21 @@ import '../style/App.css';
 import SearchBar from './SearchBar';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {session: '2017W'};
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(e) {
+		this.setState({session: e.target.innerHTML});
+	}
+
   render() {
     return (
     	<div className="container">
     		<h1>Check Your Course UBC</h1>
-    		<SearchBar/>
+    		<SearchBar session={this.state.session} handleClick={this.handleClick}/>
     	</div>
     );
   }
--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -3,11 +3,15 @@ import React from 'react';
 import '../style/SearchBar.css';
 
 function SearchBar(props) {
+	function isActive(session) {
+		return props.session === session ? 'active' : '';
+	}
+
 	return(
 		<div className="searchbar">
 			<ul id="session">
-				<li>2017 W</li>
-				<li>2018 S</li>
+				<li className={isActive('2017W')} onClick={props.handleClick}>2017W</li>
+				<li className={isActive('2018S')} onClick={props.handleClick}>2018S</li>
 			</ul>
 			<input id="user-input" type="text" placeholder="CPSC 110 101" autoFocus/>
 			<button id="search-btn">Search</button>
