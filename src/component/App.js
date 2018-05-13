38fc669254543184fb45cd9e222dc13aab0fbc5c
Zhijie Zhang <ZhijieZhang0124@gmail.com>
2018-05-12 22:55:50 -0700

    server interaction

--- src/component/App.js
+++ src/component/App.js
@@ -7,19 +7,40 @@ import SearchBar from './SearchBar';
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {session: '2017W'};
+		this.state = {
+			session: '2017W', 
+			input: ''
+		};
 		this.handleClick = this.handleClick.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	handleClick(e) {
 		this.setState({session: e.target.innerHTML});
 	}
 
+	handleChange(e) {
+		this.setState({input: e.target.value});
+	}
+
+	handleSubmit(e) {
+		let [dept, course, section] = this.state.input.split(' ');
+		let [_, year, session] = this.state.session.match(/(\d{4})(\w)/);
+
+		fetch(`http://localhost:3000/course/${dept}-${course}-${section}-${year}-${session}`)
+			.then(console.log);
+		console.log(`1`);
+	}
+
   render() {
     return (
     	<div className="container">
     		<h1>Check Your Course UBC</h1>
-    		<SearchBar session={this.state.session} handleClick={this.handleClick}/>
+    		<SearchBar session={this.state.session} 
+    							handleClick={this.handleClick}
+    							handleChange={this.handleChange}
+    							handleSubmit={this.handleSubmit}/>
     	</div>
     );
   }
--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -3,18 +3,21 @@ import React from 'react';
 import '../style/SearchBar.css';
 
 function SearchBar(props) {
-	function isActive(session) {
-		return props.session === session ? 'active' : '';
+	let {session, handleClick, handleChange, handleSubmit} = props;
+
+	function isActive(_session) {
+		return session === _session ? 'active' : '';
 	}
 
 	return(
 		<div className="searchbar">
 			<ul id="session">
-				<li className={isActive('2017W')} onClick={props.handleClick}>2017W</li>
-				<li className={isActive('2018S')} onClick={props.handleClick}>2018S</li>
+				<li className={isActive('2017W')} onClick={handleClick}>2017W</li>
+				<li className={isActive('2018S')} onClick={handleClick}>2018S</li>
 			</ul>
-			<input id="user-input" type="text" placeholder="CPSC 110 101" autoFocus/>
-			<button id="search-btn">Search</button>
+			<input id="user-input" type="text" placeholder="CPSC 110 101" autoFocus
+						onChange={handleChange}/>
+			<button id="search-btn" onClick={handleSubmit}>Search</button>
 		</div>
 	);
 }
